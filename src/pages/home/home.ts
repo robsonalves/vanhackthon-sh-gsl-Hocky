71e43213658d3cecefa02462a10eb3bd47aec36d
Daniel Adigun <daniel.adigun@digitalforte.ng>
2017-11-23 18:01:03 +0100

    Added firebase credentials and installed angularfire2 + firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,13 @@
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
