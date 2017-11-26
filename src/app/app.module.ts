464a536d2d7975ff964f98fdae93ee642300ab96
Robson Alves <robson.alves@esx.com.br>
2017-11-26 17:43:43 -0200

    save file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-import { PopupService } from './../services/popup.service';
-
->>>>>>> 611ea4deca33ee36ce305f7edcc96a6d92333197
 import { RatingComponent } from './../components/rating/rating';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { FIREBASE_CONFIG } from './firebase.credentials';
