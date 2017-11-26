b3ae4567df47d8b193a9834be1c4451741834dea
Robson Alves <robson.alves@esx.com.br>
2017-11-26 14:23:42 -0200

    removing duplicates code

--- src/models/event.model.ts
+++ src/models/event.model.ts
@@ -15,21 +15,3 @@ export interface Event{
      username : string;
      eventKey : string;  
  }
-
-export interface Event{
-    key? : string,
-    name: string,
-    eventType : string,
-    description: string,
-    date: Date,
-    imgUrl: string,        
-    comments: number,
-    likes:number,
-    liked:boolean,        
-    checkedIn:boolean
-}
- export class EventJoined{
-     username : string;
-     eventKey : string;  
- }
-
